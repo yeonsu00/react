6ebdad85dc12c51a37cb9017c8961696aa55140b
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2022-01-21 01:52:07 +0900

    엘리먼트 렌더링

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import ReactDOM from 'react-dom';
 
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez',
-};
+// const user = {
+//   firstName: 'Harper',
+//   lastName: 'Perez',
+// };
 
-function App(user) {
-  return (
-    <div className="App">
-      <div id="root">{user.firstName + ' ' + user.lastName}</div>
-    </div>
-  );
-}
+// function App(user) {
+
+//   return (
+//     <div className="App">
+//       {/* <div id="root">{user.firstName + ' ' + user.lastName}</div> */}
+//       <div id="root"></div>
+//       <div id="root2"></div>
+//     </div>
+//   );
+// }
 
 const element = (
   <h1>
-    Hello, {App(user)}!
+    {/* Hello, {App(user)}! */}
+    Hello, world!
   </h1>
 );
 
@@ -26,4 +30,15 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-export default App;
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello world</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+// export default App;
