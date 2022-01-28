7df46a0f9d4cffaa92a42ea6cb4e7f7c07b3f13e
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2022-01-29 02:07:13 +0900

    컴포넌트 추출

--- src/App.js
+++ src/App.js
@@ -42,20 +42,62 @@ import ReactDOM from 'react-dom';
 
 // setInterval(tick, 1000);
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Welcome name="Yeonsu" />
+//       <Welcome name="Sara" />
+//     </div>
+//   );
+// }
+// //const element = <Welcome name="Yeonsu" />;
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// export default App;
+
+
+/*컴포넌트 추출*/
+function Component(props) {
+  return (
+    <Comment user={props.author}/>
+  );
 }
 
-function App() {
+function Avatar(props) {
   return (
-    <div>
-      <Welcome name="Yeonsu" />
-      <Welcome name="Sara" />
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.author}/>
+      <div className="UserInfo-name">
+        {props.author.name}
+      </div>
     </div>
   );
 }
-//const element = <Welcome name="Yeonsu" />;
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// export default App;
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author}/>
+      <div className="Comment-text"> 
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
