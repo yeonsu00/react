9b3ac64e06fae4279bacfd4348f582756d9e7e1f
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2022-02-06 18:26:18 +0900

    폼 FlavorForm

--- src/App.js
+++ src/App.js
@@ -523,12 +523,43 @@ class EssayForm extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <NameForm />,
-  document.getElementById('root')
-);
+class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'coconuts'};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Your favorite flavor is: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option claue="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" vlaue="Submit" />
+      </form>
+    );
+  }
+}
 
 ReactDOM.render(
-  <EssayForm />,
-  document.getElementById('root2')
+  <FlavorForm />,
+  document.getElementById('root')
 );
