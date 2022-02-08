5ecf7c65afa5e5e662bc234ee43305702e092566
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2022-02-08 23:54:57 +0900

    State끌어올리기 Calculator2

--- src/App.js
+++ src/App.js
@@ -610,14 +610,48 @@ import React from 'react';
 
 
 /*State 끌어올리기*/
-function BoilingVerdict(props) {
-  if (props.celsius >= 100) {
-    return <p>The water would boil.</p>;
-  }
-  return <p>The water would not boil.</p>
-}
+// function BoilingVerdict(props) {
+//   if (props.celsius >= 100) {
+//     return <p>The water would boil.</p>;
+//   }
+//   return <p>The water would not boil.</p>
+// }
 
-class Calculator extends React.Component {
+// class Calculator extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleChange = this.handleChange.bind(this);
+//     this.state = {temperature: ''};
+//   }
+
+//   handleChange(e) {
+//     this.setState({temperature: e.target.value});
+//   }
+
+//   render() {
+//     const temperature = this.state.temperature;
+//     return (
+//       <fieldset>
+//         <legend>Enter temperature in Celsius:</legend>
+//         <input
+//           value={temperature}
+//           onChange={this.handleChange} />
+//         <BoilingVerdict
+//           celsius={parseFloat(temperature)} /> 
+//       </fieldset>
+//     );
+//   }
+// }
+
+
+
+
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+};
+
+class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
@@ -630,15 +664,26 @@ class Calculator extends React.Component {
 
   render() {
     const temperature = this.state.temperature;
+    const scale = this.props.scale;
     return (
       <fieldset>
-        <legend>Enter temperature in Celsius:</legend>
-        <input
-          value={temperature}
-          onChange={this.handleChange} />
-        <BoilingVerdict
-          celsius={parseFloat(temperature)} /> 
+        <legend>Enter temperature in
+          {scaleNames[scale]}:
+        </legend>
+        <input value={temperature}
+               onChange={this.handleChange} />
       </fieldset>
+    );
+  }
+}
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div>
+        <TemperatureInput scale="c" />
+        <TemperatureInput scale="f" />
+      </div>
     );
   }
 }
